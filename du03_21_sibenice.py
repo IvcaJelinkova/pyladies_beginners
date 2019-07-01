3c870c692af9fcc86285e41bb400ed37b157a44f
Ivča Jelínková <jelinkova.ivana@gmail.com>
2019-07-01 23:36:39 +0200

    gallows with repeated letter

--- du03_21_sibenice.py
+++ du03_21_sibenice.py
@@ -7,54 +7,39 @@
 # OK - Výchozí stav je řetězec s tolika podtržítky, kolik je ve vybraném slově písmen.
 # OK - Výchozí počet neúspěšných pokusů je nula.
 # Stále dokola:
-	# OK - Zeptej se hráče na písmeno.
-	# OK - Pokud je to písmeno ve vybraném slově, zaměň ve stavu příslušná podtržítka 
-		# za ono písmeno. (Bude se hodit řetězcová metoda index. a funkce zamen.)
-	# OK - Pokud dané písmeno není ve vybraném slově, započítej neúspěšný pokus.
-	# OK - Vypiš stav (slovo s případnými podtržítky).
-	# OK - Pokud už ve slově nejsou podtržítka, pogratuluj hráči a ukonči hru.
-	# OK - Vypiš počet neúspěšných pokusů a odpovídající obrázek. Funkci, která ti vrátí 
-		# obrázek podle počtu pokusů, si můžeš stáhnout z Gitu
-	# OK - Pokud je počet neúspěšných pokusů 9 (nebo víc), hráč prohrál. Dej mu to najevo a ukonči program.
+    # OK - Zeptej se hráče na písmeno.
+    # OK - Pokud je to písmeno ve vybraném slově, zaměň ve stavu příslušná podtržítka
+            # za ono písmeno. (Bude se hodit řetězcová metoda index. a funkce zamen.)
+    # OK - Pokud dané písmeno není ve vybraném slově, započítej neúspěšný pokus.
+    # OK - Vypiš stav (slovo s případnými podtržítky).
+    # OK - Pokud už ve slově nejsou podtržítka, pogratuluj hráči a ukonči hru.
+    # OK - Vypiš počet neúspěšných pokusů a odpovídající obrázek. Funkci, která ti vrátí
+    #       obrázek podle počtu pokusů, si můžeš stáhnout z Gitu
+    # OK - Pokud je počet neúspěšných pokusů 9 (nebo víc), hráč prohrál. Dej mu to najevo a ukonči program.
 
-from random import randrange
+from random import choice
 
-slovo = ''
-pocet_neuspech_pokusu = 0
-pozice_pismene = ''
+vybrane_slovo = choice(['trávník', 'stromek', 'stavení', 'čokoláda', 'ananas'])
+hadane_slovo = len(vybrane_slovo) * '_ '
+neuspesne_pokusy = 0
 
-def vyber_slovo(slovo): 
-	"""Vybere náhodně jedno ze tří slov."""
-	cislo = randrange(3)
-	if cislo == 0: 
-		slovo = 'náhody'
-	elif cislo == 1: 
-		slovo = 'lotři'
-	else: 
-		slovo = 'stavení'
-	return slovo
 
-hadane_slovo = vyber_slovo(slovo)
-#print('Vybrané slovo je:', hadane_slovo)
+def zamen(hadane_slovo, pozice, hadane_pismeno):
+    """Zaměňuje podtržítko za hádané písmeno."""
+    zacatek = hadane_slovo[ :pozice]
+    prostredek = hadane_pismeno
+    konec = hadane_slovo[pozice + 1: ]
+    return zacatek + prostredek + konec
 
-slovo_uhodnute = len(hadane_slovo) * '_'
-#print('Délka vybraného slova je:', len(slovo_uhodnute))
-print('Hádané slovo:', slovo_uhodnute)
-
-def zamen(slovo_uhodnute, pozice, nahrada):
-	zacatek	= slovo_uhodnute[ :pozice]
-	prostredek = nahrada
-	konec = slovo_uhodnute[pozice+1:]
-	return zacatek + prostredek + konec
 
 def obrazek(level):
-	"""Vykresluje obrázek na základě počtu chyb."""
-	if level == 0:
-	    return """
+    """Vykresluje obrázek na základě počtu chyb."""
+    if level == 0:
+        return """
 	    ~~~~~~~
 	    """
-	elif level == 1:
-	    return """
+    elif level == 1:
+        return """
 	    +
 	    |
 	    |
@@ -63,8 +48,8 @@ def obrazek(level):
 	    |
 	    ~~~~~~~
 	    """
-	elif level == 2:
-	    return """
+    elif level == 2:
+        return """
 	    +---.
 	    |
 	    |
@@ -73,38 +58,38 @@ def obrazek(level):
 	    |
 	    ~~~~~~~
 	    """
-	elif level == 3:
-	    return """
-	    +---.
-	    |   |
-	    |
-	    |
-	    |
-	    |
-	    ~~~~~~~
-	    """
-	elif level == 4:
-	    return """
+    elif level == 3:
+        return """
 	    +---.
 	    |   |
-	    |   O
+	    |
 	    |
 	    |
 	    |
 	    ~~~~~~~
 	    """
-	elif level == 5:
-	    return """
+    elif level == 4:
+        return """
 	    +---.
 	    |   |
 	    |   O
+	    |
+	    |
+	    |
+	    ~~~~~~~
+	    """
+    elif level == 5:
+        return """
+	    +---.
+	    |   |
+	    |   O
 	    |   |
 	    |
 	    |
 	    ~~~~~~~
 	    """
-	elif level == 6:
-	    return """
+    elif level == 6:
+        return """
 	    +---.
 	    |   |
 	    |   O
@@ -113,8 +98,8 @@ def obrazek(level):
 	    |
 	    ~~~~~~~
 	    """
-	elif level == 7:
-	    return """
+    elif level == 7:
+        return """
 	    +---.
 	    |   |
 	    |   O
@@ -123,8 +108,8 @@ def obrazek(level):
 	    |
 	    ~~~~~~~
 	    """
-	elif level == 8:
-	    return """
+    elif level == 8:
+        return """
 	    +---.
 	    |   |
 	    |   O
@@ -133,8 +118,8 @@ def obrazek(level):
 	    |
 	    ~~~~~~~
 	    """
-	else:
-	    return """
+    else:
+        return """
 	    +---.
 	    |   |
 	    |   O
@@ -145,26 +130,44 @@ def obrazek(level):
 	    """
 
 
-while True: 
-	if '_' in slovo_uhodnute:
-		if pocet_neuspech_pokusu < 9: 	
-			pismeno = input('Zadej písmeno do šibenice: ')
-			if pismeno in hadane_slovo:
-				pozice_pismene = hadane_slovo.index(pismeno)
-				#print('Zadané písmeno je na pozici:', pozice_pismene)
-				slovo_uhodnute = zamen(slovo_uhodnute, pozice_pismene, pismeno)
-				print('Hádané slovo:', slovo_uhodnute)
-			else: 
-				print('Písmeno není ve slově! ')
-				pocet_neuspech_pokusu += 1
-				print('Počet neúspěšných pokusů:', pocet_neuspech_pokusu)
-				level = pocet_neuspech_pokusu
-				print(obrazek(level))
-				print('Hádané slovo:', slovo_uhodnute)
-		else: 
-			print('\n Šibenice splnila svůj účel. Prohrál jsi.')
-			break
-	else: 
-		print('\n Gratuluji k výhře! ')
-		break
+
+def pozice_pismene(vybrane_slovo, hadane_pismeno):
+    """Vrací seznam výskytů pozic hádaného písmene ve vybraném slově."""
+    seznam_vyskytu_pismene = []
+    for znak in range(len(vybrane_slovo)):
+        if vybrane_slovo[znak] == hadane_pismeno:
+            seznam_vyskytu_pismene.append(znak)
+    return seznam_vyskytu_pismene
+
+
+print('\n\n\n' + obrazek(0))
+print(hadane_slovo)
+
+
+while True:
+    if '_' in hadane_slovo:
+        if neuspesne_pokusy < 9:
+            hadane_pismeno = input('\nHádané písmeno: ')
+            pocet_vyskytu = 0
+            if hadane_pismeno in vybrane_slovo:
+                print('Trefa! ')
+                seznam_vyskytu_pismene = pozice_pismene(vybrane_slovo, hadane_pismeno)
+                for vyskyt_pismene in seznam_vyskytu_pismene:
+                    hadane_slovo = zamen(hadane_slovo, vyskyt_pismene * 2, hadane_pismeno)  # * 2 kvůli mezerám mezi "_", pro přehlednost
+                    pocet_vyskytu += 1
+                    if pocet_vyskytu == len(seznam_vyskytu_pismene):
+                        print('\nAktuální hádané slovo:   ' + hadane_slovo)
+            else:
+                neuspesne_pokusy += 1
+                level = neuspesne_pokusy
+                print(obrazek(level))
+                print('Počet neúspěšných pokusů: ' + str(neuspesne_pokusy))
+                print('\nAktuální hádané slovo:   '+ hadane_slovo)
+        else:
+            print('\nProhrál jsi. Šibenice splnila účel. ')
+            break
+    else:
+        print('\nVyhrál jsi! Gratuluji. ')
+        break
+
 
