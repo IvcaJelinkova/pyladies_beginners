4073776b29a399fdde1c1d4a623f16bd91053d76
ivjel <jelinkova.ivana@gmail.com>
2022-09-29 18:12:44 +0200

    add circuit and volume of the circle

--- 01_vyrazy/ctverec.py
+++ 01_vyrazy/ctverec.py
@@ -1,3 +1,5 @@
+from math import pi
+
 def obvod_ctverce(strana): 
     """Program vypočítá obvod čtverce zadané strany."""
     return strana * 4
@@ -6,7 +8,18 @@ def obsah_ctverce(strana):
     """Program vypočítá obsah čtverce zadané strany."""
     return strana ** 2
 
+def obvod_a_obsah_kruhu(polomer): 
+    """Program vypočítá obvod a obsah kruhu. """
+    obvod = 2 * pi * polomer
+    obsah = pi * polomer ** 2
+    return obvod, obsah
+
+
 strana = 356
 print('Obvod čtverce se stranou', strana, 'cm je', obvod_ctverce(strana), 'cm. ') 
 print('Obsah čtverce se stranou', strana, 'cm je', obsah_ctverce(strana), 'cm2. ')
+obvod, obsah = obvod_a_obsah_kruhu(strana)
+print('Obvod kruhu s poloměrem', strana, 'cm je', obvod, 'cm a jeho obsah je', obsah, 'cm2.')
+
+
 
