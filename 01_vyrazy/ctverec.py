c111959b2a38e95c6043ba22deb7d937828db8d9
ivana.jelinkova <jelinkova.ivana@gmail.com>
2022-09-30 17:00:19 +0200

    add folder

--- 01_vyrazy/ctverec.py
+++ 01_vyrazy/ctverec.py
@@ -15,11 +15,24 @@ def obvod_a_obsah_kruhu(polomer):
     return obvod, obsah
 
 
-strana = 356
-print('Obvod čtverce se stranou', strana, 'cm je', obvod_ctverce(strana), 'cm. ') 
+# strana = 356
+strana = 0
+while True: 
+    if strana > 0: 
+        break
+    else: 
+        try: 
+            strana = int(input('Zadej délku strany/poloměru: '))
+            if strana <=0: 
+                print('Zadej kladné číslo. ')
+        except ValueError: 
+            print('Zadej kladné číslo. ')        
+    
+
+print('\nObvod čtverce se stranou', strana, 'cm je', obvod_ctverce(strana), 'cm. ') 
 print('Obsah čtverce se stranou', strana, 'cm je', obsah_ctverce(strana), 'cm2. ')
 obvod, obsah = obvod_a_obsah_kruhu(strana)
-print('Obvod kruhu s poloměrem', strana, 'cm je', obvod, 'cm a jeho obsah je', obsah, 'cm2.')
+print('Obvod kruhu s poloměrem', strana, 'cm je', obvod, 'cm a jeho obsah je', obsah, 'cm2.\n')
 
 
 
