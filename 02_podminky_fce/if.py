9d7c6302d49998bf13b0893926848b817738bcf7
ivjel <jelinkova.ivana@gmail.com>
2022-10-03 21:13:56 +0200

    new folder

---
+++ 02_podminky_fce/if.py
@@ -0,0 +1,5 @@
+side = float(input('Write a side of a square in cm: '))
+positive_number = side > 0
+
+print('Perimeter of a square with a side', side, 'is', 4 * side, 'cm')
+print('Content of a square with a side', side, 'is', side * side, 'cm2') 
