74401523c4ef53c2eed278444435aefca6172fc1
ivana.jelinkova <jelinkova.ivana@gmail.com>
2022-10-07 17:02:18 +0200

    pentagon, hexagon, heptagon and octagon

--- 03_turtle_and_cycles/turtles_creating.py
+++ 03_turtle_and_cycles/turtles_creating.py
@@ -1,9 +1,11 @@
-from turtle import forward, right, left, penup, pendown, exitonclick
+from tkinter import N
+from turtle import forward, right, left, penup, pendown, exitonclick, speed
 import math
 
+speed(10)
 left(180)
 penup()
-forward(300)
+forward(350)
 right(180)
 pendown()
 
@@ -13,7 +15,7 @@ for i in range(3):
     left(120)
 
 penup()
-forward(60)
+forward(80)
 pendown()
 
 
@@ -21,10 +23,10 @@ pendown()
 side = 30
 hypotenuse_of_triangle = math.sqrt(2 * side ** 2)
 
-b_side = math.sqrt(side ** 2 / 2)
+roof_side = math.sqrt(side ** 2 / 2)
 
 print(round(hypotenuse_of_triangle, 2))
-print(round(b_side, 2))
+print(round(roof_side, 2))
 
 for count in range(5):
     forward(side)
@@ -33,9 +35,9 @@ for count in range(5):
     right(45+90)
     forward(side)
     left(90+45)
-    forward(b_side)
+    forward(roof_side)
     left(90)
-    forward(b_side)
+    forward(roof_side)
     left(45)
     forward(side)
     left(90+45)
@@ -44,6 +46,24 @@ for count in range(5):
     forward(side)
     left(90)
     forward(20)
+
+penup()
+forward(40)
+pendown()
+
+# pentagon, hexagon, heptagon, octagon: 
+# interior angle of n-gon = 180 * (1-2/n) degrees
+# side = 200/n
+
+for number_of_sides in range(5, 9):
+    angle = 180 * (1 - 2 / number_of_sides)
+    for i in range(number_of_sides): 
+        side = 200 / number_of_sides
+        forward(side)
+        left(180 - angle)
+    penup()
+    forward(side + 45)
+    pendown()
 
 
 
