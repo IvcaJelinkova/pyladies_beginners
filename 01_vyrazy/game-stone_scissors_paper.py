5fffeab8e3b3c5da58998e6b6acaa3de282099c3
ivjel <jelinkova.ivana@gmail.com>
2022-10-19 13:31:56 +0200

    game is over when the player insert 'end'

--- 01_vyrazy/game-stone_scissors_paper.py
+++ 01_vyrazy/game-stone_scissors_paper.py
@@ -2,37 +2,50 @@
 
 from random import randrange
 
-computers_move = randrange(3)
-if computers_move == 0: 
-    computers_move = 'stone'
-elif computers_move == 1: 
-    computers_move = 'scissors'
-else: 
-    computers_move = 'paper'
+def computers_play(computers_move): 
+    """Returns computers stroke. """
+    if computers_move == 0: 
+        return 'stone'
+    elif computers_move == 1: 
+        return 'scissors'
+    else: 
+        return 'paper'
 
-possible_stroke = ['stone', 'scissors', 'paper']
+
+def game_logic(computers_move, players_move): 
+    """Returns the winner. """
+    if computers_move == 'stone': 
+        if players_move == 'scissors': 
+            return 'The Computer! '
+        else: 
+            return 'You! :-)'
+    elif computers_move == 'scissors':
+        if players_move == 'stone': 
+            return 'You! :-D'
+        else: 
+            return 'The Computer. '
+    elif computers_move == 'paper': 
+        if players_move == 'stone': 
+            return 'The Computer'
+        else: 
+            return 'You. :) '
+
+
+possible_stroke = ['stone', 'scissors', 'paper', 'end']
 players_move = ''
-while players_move not in possible_stroke: 
-    players_move = input('What is your stroke? Type: "stone", "scissors" or "paper": ')
-    if players_move not in possible_stroke: 
+while players_move != 'end':
+    computers_move = computers_play(randrange(3))
+    players_move = input('What is your stroke? Type: "stone", "scissors", "paper" or "end" for Game Over: ').lower()
+    if players_move == 'end': 
+        print('You ended this game. ')
+        break
+    elif players_move not in possible_stroke: 
         continue
     elif computers_move == players_move: 
         print('Draw! ')     # remiza
-    elif computers_move == 'stone': 
-        if players_move == 'scissors': 
-            print('The Computer wins! ')
-        else: 
-            print('You win! :-)')
-    elif computers_move == 'scissors':
-        if players_move == 'stone': 
-            print('You win! :-D')
-        else: 
-            print('The Computer wins. ')
-    elif computers_move == 'paper': 
-        if players_move == 'stone': 
-            print('The Computer wins... ')
-        else: 
-            print('You are the winner. :) ')
+    else: 
+        print('Computers move:', computers_move)
+        print(f'\nTHE WINNER IS: {game_logic(computers_move, players_move)}')
 
 
-print('Computers move:', computers_move)
+
