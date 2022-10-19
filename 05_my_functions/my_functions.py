ad0bdd56e5dc596c410ac80765a1f5e8c53a7f8c
ivjel <jelinkova.ivana@gmail.com>
2022-10-19 12:45:29 +0200

    sea_exloration with recurtion

--- 05_my_functions/my_functions.py
+++ 05_my_functions/my_functions.py
@@ -10,6 +10,20 @@ def yes_or_no(question):
             print('I do not understand! Answer "yes" or "no". ')
 
 if yes_or_no('Do you want to play a game? '): 
-    print('OK! But you have to write it. :-) ')
+    print('OK! But you have to write it. :-)\n')
 else: 
-    print('It is a pity. ')
+    print('It is a pity.\n')
+
+
+def sea_exploration(depth): 
+    """Recursive function, which prints depth and if I'm in depth lower than 30, prints that I'm diving in. Than increases a depth and call itself. Otherwise I emerge. """
+    print(f'I am looking around in depth {depth} m. ')
+    if depth >= 30: 
+        print('It\'s enough for me! ')
+    else: 
+        print(f'I\'m diving in (from {depth} m). ')
+        sea_exploration(depth + 10)
+        print(f'I\'m emerging (on {depth} m). ')
+
+sea_exploration(0)
+
