0f1d36f3083e115dba484c926135ed61d1dc8329
ivjel <jelinkova.ivana@gmail.com>
2022-10-27 22:43:10 +0200

    input validation

--- 03_turtle_and_cycles/calculator.py
+++ 03_turtle_and_cycles/calculator.py
@@ -10,8 +10,21 @@ def calculate(number1, mark, number2):
     else: 
         return 'Don\'t know this operation! '
 
-number1 = float(input('Hello, welcome to calculater \n Enter first number: '))
+
+def input_validation(query): 
+    """Validates, if the input is not a text or symbol. """
+    while True: 
+        input_result = input(query)
+        try: 
+            input_number = int(input_result)
+            return input_number
+        except ValueError: 
+            print(f'{input_result} is not a number. Please, try again. ')
+
+
+print('Hello, welcome to calculater!\n')
+number1 = input_validation('Enter the first number: ')
 mark = input('Enter operator (+, -, *, /): ')
-number2 = float(input('Hello, welcome to calculater, enter first number: '))
+number2 = input_validation('Enter the second number: ')
 
 print(number1, mark, number2, '=', calculate(number1, mark, number2))
