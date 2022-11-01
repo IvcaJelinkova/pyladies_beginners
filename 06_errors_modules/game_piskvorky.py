0480f4e8fa1f93ce027b86251e71330221889621
ivana.jelinkova <jelinkova.ivana@gmail.com>
2022-11-01 17:04:52 +0100

    new function on the players move

--- 06_errors_modules/game_piskvorky.py
+++ 06_errors_modules/game_piskvorky.py
@@ -1,5 +1,5 @@
 def rate_game(field): 
-    """Returns 'x' for the x-winner,
+    """It evaluates the game. Returns 'x' for the x-winner,
                 'o' for the o-winner, 
                 '!' for the draw, 
                 '-' for game is not over yet. """
@@ -23,12 +23,35 @@ def turn(field, position, symbol):
             if symbol in 'xo': 
                 start = field[:position]
                 end = field[position+1:]
-                return start+symbol+end
+                new_field = start+symbol+end
+                return new_field
             else: 
-                raise ValueError(f'You cannot play with {symbol}. Insert "x" or "o". ')
+                raise ValueError(f'You cannot play with "{symbol}". Insert "x" or "o". ')
         else: 
             raise ValueError(f'You cannot play to this position. Field number {position}. is full now. ')
     raise ValueError(f'You cannot play to {position}. Insert the number from 0 to 19. ')
+
+
+def players_move(field, symbol): 
+    """Asks the player to its move and returns new field. 
+    'field' is the game field, on which we play, 
+    'symbol' can be 'x' or 'o', according to which symbol the player are playing with. """
+    while True: 
+        input_position = input('To which position you want to play? ')
+        try: 
+            position = int(input_position)
+            new_field = turn(field, position, symbol)
+        except ValueError: 
+            print(f'This position is not suitable. Please, try again. ')
+        else: 
+            return new_field
+
+print(players_move('--------------------', 'x'))
+        
+
+
+
+
 
 
 
