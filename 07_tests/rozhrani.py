257a7c16403af8ac2fd3edcf69212e92645044d7
ivana.jelinkova <jelinkova.ivana@gmail.com>
2022-11-01 21:02:32 +0100

    new folder

---
+++ 07_tests/rozhrani.py
@@ -0,0 +1,14 @@
+# rozhrani.py
+
+from tkui import input, print, ano_nebo_ne, nacti_cislo
+
+# ano_nebo_ne('Do you want chocolate? ')
+
+#print(nacti_cislo())
+print('What will you do this evening. ')
+
+
+
+
+
+
