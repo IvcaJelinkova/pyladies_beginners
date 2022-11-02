da5b4babfa36eb7d34006b2cf82727000e7678fa
ivjel <jelinkova.ivana@gmail.com>
2022-11-02 13:00:39 +0100

    english version

--- 06_errors_modules/test_piskvorky.py
+++ 06_errors_modules/test_piskvorky.py
@@ -7,35 +7,35 @@ import builtins
 
 @pytest.mark.level(1)
 def test_import_rate_game():
-    """Funkce game_piskvorky.rate_game se dá naimportovat"""
+    """Function game_piskvorky.rate_game can be imported. """
     from game_piskvorky import rate_game
 
 
 @pytest.mark.level(2)
-def test_rate_game_vyhru_x():
-    """Křížky vyhrály"""
+def test_rate_game_x_won():
+    """Crosses won. """
     from game_piskvorky import rate_game
-    assert rate_game('xxx-----------------') == 'x'
+    assert rate_game('xxx-----------------') == '"x" won! Congratulation. :-) '
 
 
 @pytest.mark.level(2)
-def test_rate_game_vyhru_o():
-    """Kolečka vyhrála"""
+def test_rate_game_o_won():
+    """'o' won. """
     from game_piskvorky import rate_game
-    assert rate_game('ooo-----------------') == 'o'
+    assert rate_game('ooo-----------------') == '"o" won! Congratulation. :-) '
 
 
 @pytest.mark.level(3)
-def test_rate_game_remizu():
-    """Nastala remíza"""
+def test_rate_game_draw():
+    """It is a draw!"""
     from game_piskvorky import rate_game
-    assert rate_game('oxoxoxoxoxoxoxoxoxox') == '!'
-    assert rate_game('xxooxxooxxooxxooxxoo') == '!'
+    assert rate_game('oxoxoxoxoxoxoxoxoxox') == 'It is the draw! '
+    assert rate_game('xxooxxooxxooxxooxxoo') == 'It is the draw! '
 
 
 @pytest.mark.level(4)
-def test_rate_game_hru():
-    """Hra neskončila"""
+def test_rate_game():
+    """Play did not over. """
     from game_piskvorky import rate_game
     assert rate_game('xx----------------oo') == '-'
     assert rate_game('-xoxoxoxoxoxoxoxoxox') == '-'
@@ -47,213 +47,213 @@ def test_rate_game_hru():
 
 
 @pytest.mark.level(10)
-def test_import_tah():
-    """Funkce util.tah se dá naimportovat"""
-    from util import tah
+def test_import_turn():
+    """Function game_util.turn can be imported. """
+    from game_util import turn
 
 
 @pytest.mark.level(11)
-def test_tah_x():
-    """Pozitivní test funkce `tah` se symbolem 'x'"""
-    from util import tah
-    assert tah("--------------------", 0, 'x') == 'x-------------------'
-    assert tah("--------------------", 10, 'x') == '----------x---------'
-    assert tah("--------------------", 19, 'x') == '-------------------x'
-    assert tah("o-ox----------------", 1, 'x') == 'oxox----------------'
+def test_turn_x():
+    """Positive test of the function `turn` with symbol 'x'"""
+    from game_util import turn
+    assert turn("--------------------", 0, 'x') == 'x-------------------'
+    assert turn("--------------------", 10, 'x') == '----------x---------'
+    assert turn("--------------------", 19, 'x') == '-------------------x'
+    assert turn("o-ox----------------", 1, 'x') == 'oxox----------------'
 
 
 @pytest.mark.level(11)
-def test_tah_o():
-    """Pozitivní test funkce `tah` se symbolem 'o'"""
-    from util import tah
-    assert tah("--------------------", 0, 'o') == 'o-------------------'
-    assert tah("--------------------", 10, 'o') == '----------o---------'
-    assert tah("--------------------", 19, 'o') == '-------------------o'
-    assert tah("x-xo----------------", 1, 'o') == 'xoxo----------------'
+def test_turn_o():
+    """Positive test of the function `turn` with symbol 'o'"""
+    from game_util import turn
+    assert turn("--------------------", 0, 'o') == 'o-------------------'
+    assert turn("--------------------", 10, 'o') == '----------o---------'
+    assert turn("--------------------", 19, 'o') == '-------------------o'
+    assert turn("x-xo----------------", 1, 'o') == 'xoxo----------------'
 
 
 @pytest.mark.level(20)
-def test_tah_velka_pozice():
-    """Tah na pozici co není v poli by měl skončit chybou ValueError"""
-    from util import tah
+def test_turn_big_position():
+    """turn to the position which is not in the field could end with ValueError. """
+    from game_util import turn
     with pytest.raises(ValueError):
-        tah("--------------------", 20, 'x')
+        turn("--------------------", 20, 'x')
 
 
 @pytest.mark.level(21)
-def test_tah_zaporna_pozice():
-    """Tah na zápornou pozici by měl skončit chybou ValueError"""
-    from util import tah
+def test_turn_negative_position():
+    """turn to the negative position could end with ValueError. """
+    from game_util import turn
     with pytest.raises(ValueError):
-        tah("--------------------", -1, 'x')
+        turn("--------------------", -1, 'x')
 
 
 @pytest.mark.level(22)
-def test_tah_obsazeno():
-    """Tah na obsazené políčko by měl skončit chybou ValueError"""
-    from util import tah
+def test_turn_occupied():
+    """turn to the occupied field could end with ValueError"""
+    from game_util import turn
     with pytest.raises(ValueError):
-        tah("o-------------------", 0, 'o')
+        turn("o-------------------", 0, 'o')
     with pytest.raises(ValueError):
-        tah("----------x---------", 10, 'o')
+        turn("----------x---------", 10, 'o')
 
 
 @pytest.mark.level(23)
-def test_tah_spatny_symbol():
-    """Tah s jiným symbolem než 'o' a 'x' by měl skončit chybou ValueError"""
-    from util import tah
+def test_turn_wrong_symbol():
+    """turn with different symbol than 'o' or 'x' could end with ValueError"""
+    from game_util import turn
     with pytest.raises(ValueError):
-        tah("--------------------", 2, 'řeřicha')
+        turn("--------------------", 2, 'řeřicha')
     with pytest.raises(ValueError):
-        tah("--------------------", 2, 'm')
+        turn("--------------------", 2, 'm')
     with pytest.raises(ValueError):
-        tah("--------------------", 2, '')
+        turn("--------------------", 2, '')
 
 
 @pytest.mark.level(24)
-def test_tah_mala_pismena():
-    """Symboly 'o' a 'x' musí být malým písmenem"""
-    from util import tah
+def test_turn_small_characters():
+    """Symbols 'o' a 'x' have to be small characters. """
+    from game_util import turn
     with pytest.raises(ValueError):
-        tah("--------------------", 2, 'O')
+        turn("--------------------", 2, 'O')
     with pytest.raises(ValueError):
-        tah("--------------------", 2, 'X')
+        turn("--------------------", 2, 'X')
 
 
 @pytest.mark.level(24)
-def test_tah_spatny_symbol_ox():
-    """Tah s více symboly najednou by měl skončit chybou ValueError"""
-    from util import tah
+def test_turn_wrong_symbol_ox():
+    """turn with more symbols togetger could end with ValueError"""
+    from game_util import turn
     with pytest.raises(ValueError):
-        tah("--------------------", 2, 'xo')
+        turn("--------------------", 2, 'xo')
     with pytest.raises(ValueError):
-        tah("--------------------", 2, 'ox')
+        turn("--------------------", 2, 'ox')
 
 
 @pytest.mark.level(30)
-def test_import_tah_hrace():
-    from game_piskvorky import tah_hrace
+def test_import_turn_of_player():
+    from game_piskvorky import players_move
 
 
 @pytest.mark.level(31)
-def test_tah_hrace_x_0(podstrc_vstup):
-    """Tah hráče: X na pozici 0 na prázdné pole"""
-    from game_piskvorky import tah_hrace
-    podstrc_vstup('0')
-    assert tah_hrace('--------------------', 'x') == 'x-------------------'
+def test_players_move_x_0(push_input):
+    """turn of the player: X to the position 0 on the empty field. """
+    from game_piskvorky import players_move
+    push_input('0')
+    assert players_move('--------------------', 'x') == 'x-------------------'
 
 
 @pytest.mark.level(31)
-def test_tah_hrace_o_19(podstrc_vstup):
-    """Tah hráče: O na pozici 19 na prázdné pole"""
-    from game_piskvorky import tah_hrace
-    podstrc_vstup('19')
-    assert tah_hrace('--------------------', 'o') == '-------------------o'
+def test_players_move_o_19(push_input):
+    """turn hráče: O na pozici 19 na prázdné field"""
+    from game_piskvorky import players_move
+    push_input('19')
+    assert players_move('--------------------', 'o') == '-------------------o'
 
 
 @pytest.mark.level(32)
-def test_tah_hrace_necislo(podstrc_vstup):
-    """Tah hráče: odpověď 'řeřicha' není uznána, zeptá se znova"""
-    from game_piskvorky import tah_hrace
-    podstrc_vstup('řeřicha', '2')
-    assert tah_hrace('--------------------', 'o') == '--o-----------------'
+def test_players_move_not_number(push_input):
+    """turn of the player: answer 'řeřicha' is not corrected, will ask again. """
+    from game_piskvorky import players_move
+    push_input('řeřicha', '2')
+    assert players_move('--------------------', 'o') == '--o-----------------'
 
 
 @pytest.mark.level(33)
-def test_tah_hrace_obsazeno(podstrc_vstup):
-    """Tah hráče: plné pole není uznáno, zeptá se znova"""
-    from game_piskvorky import tah_hrace
-    podstrc_vstup('0', '1')
-    assert tah_hrace('x-------------------', 'o') == 'xo------------------'
+def test_players_move_occupied(push_input):
+    """turn of the player: full field is not corrected, will ask again. """
+    from game_piskvorky import players_move
+    push_input('0', '1')
+    assert players_move('x-------------------', 'o') == 'xo------------------'
 
 
 @pytest.mark.level(34)
-def test_tah_hrace_ukazka(podstrc_vstup):
-    """Tah hráče: test ukázky ze zadání"""
-    from game_piskvorky import tah_hrace
-    podstrc_vstup('nevím', '0', '-1', '151', '2')
-    assert tah_hrace('o-------------------', 'x') == 'o-x-----------------'
+def test_players_move_example(push_input):
+    """turn of the player: test of the example from the entry. """
+    from game_piskvorky import players_move
+    push_input('don\t know', '0', '-1', '151', '2')
+    assert players_move('o-------------------', 'x') == 'o-x-----------------'
 
 
 @pytest.mark.level(40)
-def test_import_tah_pocitace():
-    from ai import tah_pocitace
+def test_import_computers_turn():
+    from game_ai import computers_move
 
 
 @pytest.mark.level(41)
-def test_tah_pocitace_prazdne():
-    """Tah počítače na prázdné pole"""
-    from ai import tah_pocitace
+def test_computers_move_prazdne():
+    """turn of the computer on the empty field"""
+    from game_ai import computers_move
     for symbol in 'ox':
-        result = tah_pocitace('-' * 20, symbol)
-        assert len(result) == 20, result + ': Špatná délka pole'
-        assert result.count('-') == 19, result + ': Špatný počet pomlček'
-        assert result.count(symbol) == 1, result + ': Špatný počet symbolů ' + symbol
+        result = computers_move('-' * 20, symbol)
+        assert len(result) == 20, result + ': Wrong lengt of the field'
+        assert result.count('-') == 19, result + ': Wrong number of dashes'
+        assert result.count(symbol) == 1, result + ': Wrong number of symbols ' + symbol
 
 
 @pytest.mark.level(42)
-def test_tah_pocitace_plne():
-    """Tah počítače na plné pole musí vyvolat výjimku"""
-    from ai import tah_pocitace
+def test_computers_move_full():
+    """turn of the computer to the full field have to raise ValueError"""
+    from game_ai import computers_move
     for symbol in 'ox':
         with pytest.raises(ValueError):
-            result = tah_pocitace('x' * 20, symbol)
+            result = computers_move('x' * 20, symbol)
 
 
 @pytest.mark.level(43)
-def test_tah_pocitace_skoro_plne():
-    """Tah počítače na skoro plné pole (volno uprostřed)"""
-    from ai import tah_pocitace
-    pole = 'xoxoxoxoxo-xoxoxoxox'
-    result = tah_pocitace(pole, 'o')
-    # Je jen 1 možnost kam hrát.
+def test_computers_move_almost_full():
+    """turn of the computer on almost full field (free in the middle)"""
+    from game_ai import computers_move
+    field = 'xoxoxoxoxo-xoxoxoxox'
+    result = computers_move(field, 'o')
+    # There is only one possibility to which field play.
     assert result == 'xoxoxoxoxooxoxoxoxox'
 
 
 @pytest.mark.level(43)
-def test_tah_pocitace_skoro_plne_zacatek():
-    """Tah počítače na skoro plné pole (volno na začátku)"""
-    from ai import tah_pocitace
-    pole = '-xoxoxoxoxoxoxoxoxox'
-    result = tah_pocitace(pole, 'o')
-    # Je jen 1 možnost kam hrát.
+def test_computers_move_almost_full_start():
+    """turn of the computer to almost full field (free in the start)"""
+    from game_ai import computers_move
+    field = '-xoxoxoxoxoxoxoxoxox'
+    result = computers_move(field, 'o')
+    # There is only one possibility to play.
     assert result == 'oxoxoxoxoxoxoxoxoxox'
 
 
 @pytest.mark.level(43)
-def test_tah_pocitace_skoro_plne_konec():
-    """Tah počítače na skoro plné pole (volno na konci)"""
-    from ai import tah_pocitace
-    pole = 'oxoxoxoxoxoxoxoxoxo-'
-    result = tah_pocitace(pole, 'x')
-    # Je jen 1 možnost kam hrát.
+def test_computers_move_almost_full_end():
+    """turn of the computer to almost full field (free in the end)"""
+    from game_ai import computers_move
+    field = 'oxoxoxoxoxoxoxoxoxo-'
+    result = computers_move(field, 'x')
+    # There is only one possibility to play.
     assert result == 'oxoxoxoxoxoxoxoxoxox'
 
 @pytest.mark.level(43)
-def test_tah_pocitace_skoro_plne_konec2():
-    """Tah počítače na skoro plné pole (2× volno na konci)"""
-    from ai import tah_pocitace
-    pole = 'xooxxooxoxoxoxooxx--'
-    result = tah_pocitace(pole, 'x')
-    assert len(result) == 20, result + ': Špatná délka pole'
-    assert result.count('x') == 10, result + ': Špatný počet symbolů x'
-    assert result.count('o') == 9, result + ': Špatný počet symbolů o'
+def test_computers_move_almost_full_end2():
+    """turn of the computer to almost full field (2times free in the end)"""
+    from game_ai import computers_move
+    field = 'xooxxooxoxoxoxooxx--'
+    result = computers_move(field, 'x')
+    assert len(result) == 20, result + ': Wrong length of field'
+    assert result.count('x') == 10, result + ': Wrong number of symbols x'
+    assert result.count('o') == 9, result + ': Wrong number of symbols o'
 
 
 @pytest.fixture
-def podstrc_vstup(monkeypatch):
-    """Podstrčí funkci input daný vstup, tak jako by ho zadal uživatel."""
+def push_input(monkeypatch):
+    """Push input to the function like users gave it."""
     # Tohle je trochu pokročilá testovací magie.
     # Viz pokročilý kurz: https://naucse.python.cz/course/mi-pyt/intro/testing/
-    vstup = []
-    def _podstrc(*args):
-        vstup.extend(args)
-    def podstrceny_input(otazka):
-        assert vstup != [], 'Program by neměl pokládat další otázku'
-        odpoved = vstup.pop(0)
-        print(otazka + odpoved)
-        return odpoved
-    monkeypatch.setattr(builtins, 'input', podstrceny_input)
-    yield _podstrc
-    # Kontrola, že všechno ze vstupu bylo přečteno:
-    assert vstup == [], 'Nepřečtené odpovědi'
+    input = []
+    def _give_input(*args):
+        input.extend(args)
+    def pushed_input(question):
+        assert input != [], 'Program should not ask on the other question. '
+        answer = input.pop(0)
+        print(question + answer)
+        return answer
+    monkeypatch.setattr(builtins, 'input', pushed_input)
+    yield _give_input
+    # Kontrola, že všechno ze inputu bylo přečteno:
+    assert input == [], 'Unread answers'
